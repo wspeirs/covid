6e8bee5d6514764c9d1645f58e893ff0b6f36b89
William Speirs <bill.speirs@gmail.com>
2020-03-29 16:22:57 -0400

    Created script to fetch data

---
+++ fetch_data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p data
+wget 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' -O data/global_deaths.csv
+wget 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv' -O data/us_deaths.csv
+
