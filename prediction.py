bbf256d440a2917abb05b6f61337181229f3d0bd
William Speirs <bill.speirs@gmail.com>
2020-04-03 11:45:24 -0400

    Updated prediction script to auto generate index file

--- prediction.py
+++ prediction.py
@@ -5,9 +5,9 @@ import matplotlib.dates as mdates
 import datetime
 
 from math import pi, sin
-from compute_us import compute_us
 from dateutil.parser import parse
 from collections import defaultdict
+from jinja2 import Template
 
 
 # ensure we can print all rows
@@ -65,7 +65,6 @@ def fit_sin(df, country, lockdown_date):
 
             return last_death_date
 
-
 countries = [
     # "Belgium",
      # "Brazil",
@@ -74,20 +73,18 @@ countries = [
     # "Germany",
      # "Indonesia",
     # "Iran",
-    # "Italy",
+    ("Italy", datetime.date(year=2020, month=3, day=8) ),
     # "Korea, South",
      # "Netherlands",
      # "Portugal",
-    # "Spain",
+    ("Spain", datetime.date(year=2020, month=3, day=15) ),
      # "Sweden",
      # "Switzerland",
      # "Turkey",
-    # "United Kingdom",
+    ("United Kingdom", datetime.date(year=2020, month=3, day=24) )
 ]
 
 global_deaths_df = pd.read_csv('data/global_deaths.csv', header=0)
-
-us_df = compute_us()
 
 data = defaultdict(lambda: dict())
 
@@ -95,45 +92,59 @@ for date in global_deaths_df.columns[4:]:
     date_dt = parse(date).date()
 
     for country, df in global_deaths_df.groupby('Country/Region'):
-        if country not in countries:
+        if country not in set(map(lambda t: t[0], countries)):
             continue
 
         data[country][date_dt] = df[date].sum()
 
-    if date_dt in us_df.index:
-        data['US'][date_dt] = us_df.loc[date_dt]['deaths']
-
 df = pd.DataFrame(data)
 df = df.sort_index()
 
-print(df)
+predictions = defaultdict(lambda : dict())
 
-df_diff = df.copy()
+# go through dates to generate predictions through time
+for data_date in global_deaths_df.columns[73:]:
+    data_date = parse(data_date).date()  # convert to datetime
 
-# convert from raw counts to percent change with a 3-day moving average
-for country in list(countries) + ['US']:
-    df_diff[country] = df[country].diff().rolling(window=3).mean()
+    # go through each country and lockdown date
+    for country, lockdown_date in list(countries):
+        df_diff = pd.DataFrame(df[country])
+        df_diff = df_diff[df_diff.index <= data_date]
 
-df_diff = df_diff.fillna(value=0)
-print(df_diff)
+        # convert from raw counts to difference with a 3-day moving average
+        df_diff[country] = df[country].diff().rolling(window=3).mean()
 
-# fit the sin curve
-lockdown_date = datetime.date(year=2020, month=3, day=24)
-clear_date = fit_sin(df_diff, 'US', lockdown_date)
+        df_diff = df_diff.fillna(value=0)
+        # print(df_diff)
 
-print("Clear by: {} with {} deaths".format(clear_date, df_diff['computed'].sum()))
+        # fit the sin curve
+        clear_date = fit_sin(df_diff, country, lockdown_date)
+        deaths = df_diff['computed'].sum()
 
-# plot the results
-# ax = df_diff.plot(figsize=(10,5), title='Deaths by Day')
-ax = df_diff.plot(figsize=(10,5))
-(start, end) = ax.get_xlim()
-ax.xaxis.set_ticks(np.arange(start, end, 6))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%-m/%-d'))
-plt.ylabel('Deaths by Day')
-# plt.ylim(-1, 6)
-plt.grid(which='both', axis='x')
+        print("Clear by: {} with {} deaths".format(clear_date, deaths))
 
-# plt.show()
-plt.savefig('site/img/us.png')
+        # record the prediction
+        predictions[data_date][country] = '{} - {:,}'.format(clear_date.strftime('%B %-d'), deaths)
 
-print()
+        # plot the results if it's the last date
+        if data_date == parse(global_deaths_df.columns[-1]).date():
+            ax = df_diff.plot(figsize=(10,5))
+            (start, end) = ax.get_xlim()
+            ax.xaxis.set_ticks(np.arange(start, end, 6))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%-m/%-d'))
+            plt.ylabel('Deaths by Day')
+            plt.grid(which='both', axis='x')
+
+            # plt.show()
+            plt.savefig('site/img/{}.png'.format(country.lower().replace(' ', '_')))
+
+# convert from dictionary to sorted tuple list
+predictions = [(d, predictions[d]) for d in sorted(predictions.keys(), reverse=True)]
+print(predictions)
+
+with open('site/index.template', 'r') as f:
+    t = Template(f.read())
+    out = t.render(predictions=predictions)
+
+    with open('site/index.html', 'w') as f_out:
+        f_out.write(out)
